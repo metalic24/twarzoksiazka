97c9f1dba1e88ddd5d423be82ee26d75beb820b1
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-12-31 19:49:37 +0100

    lekkie poprawki po zmianach

--- twarzoksiazka/rej_log/urls.py
+++ twarzoksiazka/rej_log/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('hello/',views.hello_login, name='hello'),
 
     path('login/',views.viev_login, name='login'),
+    path('logout/',views.log_out, name='log-out'),
     path('update/',views.update_user_details, name='update'),
     path('search/',views.get_profiles, name='profile-search'),
     path('add_friend/',views.add_friend, name='add-friend'),
